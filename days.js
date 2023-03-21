7ffd13375cf52e38a7d1f711eb50b4a7c57d3134
mino225 <agnes.ohman@elev.ntig.se>
2023-03-21 14:56:15 +0100

    skissat layout, börjat jobba med att den ska hålla reda på dagarna

---
+++ days.js
@@ -0,0 +1,13 @@
+var d = new Date();
+const months = ["JANUARI", "FEBRUARI", "MARS", "APRIL", "MAJ", "JUNI", "JULI", "AUGUSTI", "SEPTEMBER", "OKTOBER", "NOVEMBER", "DECEMBER"];
+
+let day = d.getDate();
+let month = months[d.getMonth()];
+
+document.getElementById("todaysMonth").innerHTML = month;
+document.getElementById("todaysDate").innerHTML = day;
+
+if (document.getElementById("dates").innerHTML == day)
+{
+    document.getElementById("dates").style.backgroundColor = "red";
+}
