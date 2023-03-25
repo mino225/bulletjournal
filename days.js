10b929505dde4e931f9cf976d05e0ad943e6e436
mino225 <agnes.ohman@elev.ntig.se>
2023-03-25 20:30:58 +0100

    fixat att den döljer olika många dagar beroende på vilken månad det är

--- days.js
+++ days.js
@@ -4,22 +4,24 @@ const months = ["Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "
 const weekdays = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "Söndag"];
 
 let day = d.getDate();
+let dayStr = day.toString();
 let month = months[d.getMonth()];
+let monthStr = month.toString();
 let weekday = weekdays[d.getDay()-1];
 
 document.getElementById("todaysWeekday").innerHTML = weekday + "\xa0";
 document.getElementById("todaysDate").innerHTML = day + "\xa0";
 document.getElementById("todaysMonth").innerHTML = month;
 
-if (month.getAttribute("value") == "Mars") {
-    document.getElementsById("date29").style.display = "none";
+if (monthStr.toLowerCase() == "februari") {
+    document.getElementById("date29").style.display = "none";
     document.getElementById("date30").style.display = "none";
     document.getElementById("date31").style.display = "none";
-    //alert("h")
 }
 
-else if (month == "April" || month == "Juni" || month == "September" || month == "November") {
-    document.getElementsByClassName("31").innerHTML = "-";
+else if (monthStr.toLowerCase() == "april" || monthStr.toLowerCase() == "juni" || monthStr.toLowerCase() == "september" || monthStr.toLowerCase() == "november") {
+    document.getElementById("date31").style.display = "none";
+
     //alert("g")
 }
 
