46b318015ed1ae850b9d9fa765842775c3750b58
mino225 <agnes.ohman@elev.ntig.se>
2023-03-26 19:13:50 +0200

    jobbat med att visa rätt antal blanka dagar innan

--- days.js
+++ days.js
@@ -9,14 +9,33 @@ let month = months[d.getMonth()];
 let monthStr = month.toString();
 let weekday = weekdays[d.getDay()-1];
 
+const day29 = document.getElementById("date29");
+const day30 = document.getElementById("date30");
+const day31 = document.getElementById("date31");
+
 document.getElementById("todaysWeekday").innerHTML = weekday + "\xa0";
 document.getElementById("todaysDate").innerHTML = day + "\xa0";
 document.getElementById("todaysMonth").innerHTML = month;
 
+let firstDateOfMonth = day % 7
+let firstWeekdayOfMonth = d.getDay() - firstDateOfMonth;
+if (firstWeekdayOfMonth < 0) {
+    firstWeekdayOfMonth += 7;
+}
+
+if (firstWeekdayOfMonth >= 5) {
+    document.getElementById("datesBlankBeforeSix").style.display = "none";
+    document.getElementById("datesBlankBeforeFive").style.display = "none";
+}
+// alert(weekdays[firstWeekdayOfMonth]);
+
 if (monthStr.toLowerCase() == "mars") {
-    document.getElementById("date29").innerHTML = "";
-    document.getElementById("date30").innerHTML = "";
-    document.getElementById("date31").innerHTML = "";
+    day29.innerHTML = "";
+    day29.style.border = "none";
+    day30.innerHTML = "";
+    day30.style.border = "none";
+    day31.innerHTML = "";
+    day31.style.border = "none";
 }
 
 else if (monthStr.toLowerCase() == "april" || monthStr.toLowerCase() == "juni" || monthStr.toLowerCase() == "september" || monthStr.toLowerCase() == "november") {
