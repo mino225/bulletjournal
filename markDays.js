bc6248f5e0beccf49993a99b1d96af978a7fa085
mino225 <agnes.ohman@elev.ntig.se>
2023-03-27 21:08:13 +0200

    kan ta bort färgerna

--- markDays.js
+++ markDays.js
@@ -4,20 +4,24 @@
 //   this.style.backgroundColor='red'
 // }
 number = 0;
-colors = ["rgb(255, 156, 156)", "rgb(255, 80, 80)", "rgb(179, 10, 10)", "rgb(77, 3, 3)"];
+colors = ["white", "rgb(255, 156, 156)", "rgb(255, 80, 80)", "rgb(179, 10, 10)", "rgb(77, 3, 3)"];
 
 function changeColorHappiest() {
-  number = 0;
-}
-
-function changeColorHappy() {
   number = 1;
 }
 
-function changeColorOk() {
+function changeColorHappy() {
   number = 2;
 }
 
-function changeColorSad() {
+function changeColorOk() {
   number = 3;
+}
+
+function changeColorSad() {
+  number = 4;
+}
+
+function changeColorRemove() {
+  number = 0;
 }
