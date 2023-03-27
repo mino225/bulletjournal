345bff38d427ec67a7e5cb6d65a1c98f78fb4deb
mino225 <agnes.ohman@elev.ntig.se>
2023-03-27 20:58:37 +0200

    kan markera dagar med olika färger

--- markDays.js
+++ markDays.js
@@ -1,4 +1,23 @@
 // document.getElementById("test").onclick = function() {markDay()};
-function markDay() {
-  document.getElementById("allDates").style.backgroundColor = "red";
+// function markDay() {
+//   //document.getElementById("allDates").style.backgroundColor = "red";
+//   this.style.backgroundColor='red'
+// }
+number = 0;
+colors = ["rgb(255, 156, 156)", "rgb(255, 80, 80)", "rgb(179, 10, 10)", "rgb(77, 3, 3)"];
+
+function changeColorHappiest() {
+  number = 0;
+}
+
+function changeColorHappy() {
+  number = 1;
+}
+
+function changeColorOk() {
+  number = 2;
+}
+
+function changeColorSad() {
+  number = 3;
 }
