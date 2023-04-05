aaddd5443d3c8346a1b05746563dea4038d64ca9
mino225 <agnes.ohman@elev.ntig.se>
2023-04-05 11:16:41 +0200

    börjat med att den ska spara rätt värde i cookien

--- saveInformation.js
+++ saveInformation.js
@@ -1,12 +1,46 @@
+let firstDateValue;
+
 function saveData() {
     let textarea = document.getElementById("writeOne");
     textarea.style.backgroundColor = "red";
     let value = textarea.value;
     // let expiresDate = newDate();
     // expiresDate.setTime(expiresDate.getTime()+ 7*24*60*60*1000);
-    document.cookie = "writtenInFirst=" + encodeURIComponent(value);
+    
+    firstDateValue = "writtenInFirst=" + encodeURIComponent(value);
+    let firstDate = document.getElementById("writeOne");
+
+    document.cookie = getCookie(firstDateValue);
+    // firstDate.value = getCookie(firstDateValue);
+    
+    // document.cookie = "writtenInFirst=" + encodeURIComponent(value);
     
 }
+
+function getCookie(cookieName) {
+    let name = cookieName.split("=");
+    alert(name[1]);
+    return name[1];
+}
+
+let firstDate = document.getElementById("writeOne");
+firstDate.value = getCookie(firstDateValue);
+// firstDate.value = encodeURIComponent(document.cookie);
+
+// function getCookie(name) {
+//     let ca = name.split(';');
+//     for(let i = 0; i <ca.length; i++) {
+//         let c = ca[i];
+//         while (c.charAt(0) == ' ') {
+//           c = c.substring(1);
+//         }
+//         if (c.indexOf(name) == 0) {
+//           return c.substring(name.length, c.length);
+//         }
+//       }
+//       return "";
+//     }
+
 
 // function getCookie(name) {
 //     let cookies = document.cookie.split(";");
@@ -17,10 +51,22 @@ function saveData() {
 //         }
 //     }
 // }
+// function getCookie(cname) {
+//     let name = cname + "=";
+//     let decodedCookie = decodeURIComponent(document.cookie);
+//     let ca = decodedCookie.split(';');
+//     for(let i = 0; i <ca.length; i++) {
+//       let c = ca[i];
+//       while (c.charAt(0) == ' ') {
+//         c = c.substring(1);
+//       }
+//       if (c.indexOf(name) == 0) {
+//         return c.substring(name.length, c.length);
+//       }
+//     }
+//     return "";
+//   }
 
-
-let firstDate = document.getElementById("writeOne");
-firstDate.value = encodeURIComponent(document.cookie);
 
 // firstDate.value = getCookie(firstDate);
 // firstDate.innerHTML = firstDate.value;
